3b75255661342760a62100520f84de4475d7f39f
K.Vijayan <k.vijayan@diamond.ac.uk>
2021-02-24 17:01:38 +0000

    New commit 31

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
         System.out.println("Hello, World");
         System.out.println("Hello, World2");
         System.out.println("Hello, World- Version 0.0.4-SNAPSHOT");
+        System.out.println("Hello, World- Moving to New Version 0.0.7-SNAPSHOT");
     }
 
 }
